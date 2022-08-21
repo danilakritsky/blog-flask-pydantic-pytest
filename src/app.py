df0d29ba57affdf33bafa1395e4fae824bef806f
Danila Kritsky <danilakritsky@gmail.com>
2022-08-21 07:59:22 +0000

    Add a route for creating articles

--- src/app.py
+++ src/app.py
@@ -1,12 +1,14 @@
 from crypt import methods
 from urllib import request
-from flask import Flask
+from flask import Flask, jsonify, request
 
 from src.commands import CreateArticleCommand
 from src.queries import GetArticleByIdQuery, ListArticlesQuery
 
 app = Flask(__name__)
 
-@app.route('/', methods=["POST"])
+@app.route('/articles/', methods=["POST"])
 def create_article():
-    pass
+    article = CreateArticleCommand(**request.json)()
+    # return the dict representation of an object to convert to JSON
+    return jsonify(article.dict())
--- tests/test_app.py
+++ tests/test_app.py
@@ -10,13 +10,13 @@ from src.models import Article
 
 SCHEMAS_DIR = pathlib.Path(__file__).parent / 'schemas'
 
-def validate_payload(payload, schema):
-    schema = json.loads(SCHEMAS_DIR / schema)
+def validate_payload(payload, schema_name):
+    schema = json.load(open(SCHEMAS_DIR / schema_name))
     jsonschema.validate(
         payload,
         schema,
         resolver=jsonschema.RefResolver(
-            base_uri=f'file://{SCHEMAS_DIR / schema}',
+            base_uri=f'file://{SCHEMAS_DIR / schema_name}',
             referrer=schema
         )
     )
@@ -41,6 +41,7 @@ def test_create_article(client):
         "title": "New Article",
         "content": "This is a new article"
     }
+
     response = client.post(
         '/articles/',
         data=json.dumps(article),
